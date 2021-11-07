5fcfef66905424e8d47c7057132365715b81edbc
satokawa1130tomok <80613532+satokawa1130tomok@users.noreply.github.com>
2021-11-07 16:07:26 +0900

    2021/11/07

--- Assets/@Scenes/game.cs
+++ Assets/@Scenes/game.cs
@@ -4,39 +4,54 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class game : MonoBehaviourPunCallbacks
 {
-    public void OnClick()
-    {
-        
-        PhotonNetwork.ConnectUsingSettings();
-    }
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Button>().onClick.AddListener(StartGame);
-        //PhotonNetwork.ConnectUsingSettings();
-    }
+        gameObject.GetComponent<Button>();
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("マスターサバ");
+        PhotonNetwork.JoinLobby();
 
-    // Update is called once per frame
-    void StartGame()
-    {
-        Debug.Log("ルームに接続中…");
-        PhotonNetwork.JoinRandomRoom();
+
+
     }
+    public override void OnJoinedLobby() => Debug.Log("ロビー");
 
     void OnGUI()
     {
         //ログインの状態を画面上に出力
         GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
-    }
 
-    public override void OnConnectedToMaster()
+    }
+   
+    public void OnClick()
     {
         Debug.Log("ルーム作成中…");
         // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
         PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+        //PhotonNetwork.ConnectUsingSettings();
+
+        //Debug.Log("ルームに接続中…");
+        //PhotonNetwork.JoinRandomRoom();
+        
+
+    }
+
+
+    // Update is called once per frame
+
+
+
+
+    public override void OnConnectedToMaster()
+    {
+        //Debug.Log("ルーム作成中…");
+        //// "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        //PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
 
     }
 
@@ -44,8 +59,8 @@ public class game : MonoBehaviourPunCallbacks
     {
         Debug.Log("Joined");
         //キャラクターを生成
-        GameObject monster = PhotonNetwork.Instantiate("Tank", Vector3.zero, Quaternion.identity, 0);
-
+        //GameObject monster = PhotonNetwork.Instantiate("Tank", Vector3.zero, Quaternion.identity, 0);
+        SceneManager.LoadScene("PlayScene");
 
     }
 }
